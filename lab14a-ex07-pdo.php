e06d40f5362b497ce0a6cb2249d0db74fa31a8f7
Ryan J. Wong <rwong210@mtroyal.ca>
2020-11-21 15:40:10 -0700

    working on ex07

--- lab14a-ex07-pdo.php
+++ lab14a-ex07-pdo.php
@@ -1,8 +1,11 @@
-
+<?php require_once('config.inc.php'); ?>
 <!DOCTYPE html>
 <html>
 <body>
 <h1>Database Tester (PDO)</h1>
-
+    <?php
+        try {
+            $pdo
+        }
 </body>
 </html>
