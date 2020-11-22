506f6f92172e328049bf3cb69ae39c8d8d007a6c
Ryan J. Wong <rwong210@mtroyal.ca>
2020-11-22 13:21:52 -0700

    finished ex08

--- config.inc.php
+++ config.inc.php
@@ -3,6 +3,7 @@ define('DBHOST', 'localhost');
 define('DBNAME', 'art');
 define('DBUSER', 'root');
 define('DBPASS', '');
+// define('DBCONNSTRING', 'sqlite:./databases/art.db');
 // location of database(host= .DBHOST)', type of database(mysql), name of database(dbname = .DBNAME)
 define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
 ?>
